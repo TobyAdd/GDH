0a8b316a99f16e3b7e43849067611a1dfbf574f2
TobyAdd <66429886+TobyAdd@users.noreply.github.com>
2025-02-04 10:01:00 +0200

    opps

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <cctype>
+#include <fstream>
 
 namespace utilsH {
     bool isNumeric(const std::string& str);
@@ -11,7 +12,9 @@ namespace utilsH {
 }
 
 static void unimplemented() {}
-inline void logMessage(const std::string& message) {
+
+#include "config.hpp"
+static void logMessage(const std::string& message) {
     std::ofstream logFile(folderPath / "h264_encode.txt", std::ios::app);
     if (logFile.is_open()) {
         logFile << message << std::endl;
