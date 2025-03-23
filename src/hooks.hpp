3d9ff9bfe39dde7945ff3bdbe1696ae662586b8f
Andrew <kezin636@gmail.com>
2025-03-24 00:10:33 +0300

    abobid please build

--- src/hooks.hpp
+++ src/hooks.hpp
@@ -7,6 +7,8 @@ namespace hooksH {
     extern std::vector<StartPosObject*> startPositions;
     void switchStartPos(int incBy, bool direction = true);
 
+#ifdef GEODE_IS_WINDOWS
     bool isWine();
     void spawn_deafen_process(int xkeycode);
+#endif
 }
